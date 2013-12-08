97fdc866493a4d2e3cc30636121f366b7477e4eb
Ely Lucas <ely@meta-tek.net>
2013-12-08 09:56:00 -0700

    adding missing files

---
+++ AngularJSMVC/app/directives.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+    
+    var app = angular.module('tweetApp');
+
+    app.directive('tweet', function() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'app/views/tweet.html'
+        };
+    });
+})();
